d9c45f1e40dba9c1a6c89fe2b1ed74f6d2d491db
hujp <ms.20210722@gmail.com>
2023-01-26 20:51:57 +0100

    7-1 課題Java

---
+++ 7/7-1.js
@@ -0,0 +1,28 @@
+//課題1
+
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+function isEven() {
+    for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 == 0){
+        console.log(numbers[i] + 'は偶数です');
+    }
+    i++;
+}
+}
+isEven();
+
+//課題2
+
+class car{
+    constructor(gas,num){
+      this.gas = gas;
+      this.num = num;
+    }
+    getNumGas(){
+        console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です。`)
+    }
+}
+
+let kuruma = new car(100, 12345);
+kuruma.getNumGas();
