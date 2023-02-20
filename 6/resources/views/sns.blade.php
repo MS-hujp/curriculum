03b6e1f112923a737cb5f80f2f0bb7583b62ef77
hujp <ms.20210722@gmail.com>
2023-02-20 12:07:29 +0100

    6章チェックテスト(ver.2)

--- 6/resources/views/sns.blade.php
+++ 6/resources/views/sns.blade.php
@@ -1,39 +1,6 @@
 <style>
-header {
-    width: auto; 
-    height: 80px;
-    background: white; /* 背景色にグレーを指定 */
-    /* position: fixed; ウィンドウを基準に画面に固定 */
-    top: 0; 
-    left: 250px;
-    right: 250px;
-    display: flex;
-    align-items: center;
-    padding-left: 100px;
-    display: none;
-}
-
-header .menu li {
-    list-style: none; /* リストの[・]を消す */
-}
-
-header .menu li + li {
-    margin-left: 40px; /* メニューそれぞれに間隔をあけるため */
-}
-.mutter{
-    margin-right: 0px;
-    display: none;
-}
 body{
     background: #f5f5f5;
-}
-.spacer{
-    height: 80px;
-    background: #f5f5f5;
-}
-.menu{
-    margin-left: 400px;
-    display: none;
 }
 .user_name{
     padding-right: 10px;
@@ -57,8 +24,19 @@ body{
     display: flex;
     flex-flow: column;
 }
-.form-control{
-    width: 300px;
+.home{
+    margin-left:10px;
+    margin-top:10px;
+}
+.b{
+    display: flex;
+    justify-content: right;
+    margin-right: 20px;
+}
+.tsubub{
+    width: 100px;
+    margin-left: 20px;
+    margin-bottom: 10px;
 }
 .tsubuyaki{
     position: center;
@@ -71,36 +49,33 @@ body{
 
 
 @extends('layouts.app')
-{{-- layouts/admin.blade.phpを読み込む --}}
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
-
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('title', '')
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-            <h2>ニュース新規作成</h2>
+            <h2></h2>
 
 
                 <form class="card" action="{{ action('SnsController@create') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
+                         <ul>
+                             @foreach($errors->all() as $e)
+                                 <li>{{ $e }}</li>
+                             @endforeach
+                         </ul>
                     @endif
-                    <div>ホーム</div>
-                    <div class="form-group row">
+                     <div class="home">ホーム</div>
+                      <div class="form-group row">
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="1" placeholder="いまどうしてる？">{{ old('body') }}</textarea>
+                            <textarea class="col-sm-12 col-form-label ml-5" name="body" rows="1" placeholder="いまどうしてる？">{{ old('body') }}</textarea>
                         </div>
-                    </div>
+                      </div>
                     <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                     {{ csrf_field() }}
-                    <input type="submit" class="btn btn-secondary" value="つぶやく">
+                    <div class="b">
+                    <input type="submit" class="tsubub" value="つぶやく">
+                    </div>
                 </form>
 
 
@@ -108,26 +83,23 @@ body{
             <div class="tsubuyaki">
                  @foreach($posts as $post)
                   @foreach($users as $user)
-                  @if($user->id == $post->user_id)
+                   @if($user->id == $post->user_id)
                 <div class="card p-3">
                     <div>{{ $user->name }}</div>
                     <div class="text-right">{{ $post->created_at }}</div>
                     <div>{{ $post->body }}</div>
                  
                     @if(Auth::id() == $post->user_id)
-                    <div class="text-right">
-                    <a href="{{ action('SnsController@delete', ['id' => $post->id]) }}">削除</a>
-                </div>
-                </div>
-                    @endif         
+                      <div class="text-right">
+                         <a href="{{ action('SnsController@delete', ['id' => $post->id]) }}">削除</a>
+                      </div>
+                    @endif
+                </div>         
                    @endif
                   @endforeach
                  @endforeach
-
-            </div>
             </div>
         </div>
     </div>
-
 @endsection
 
